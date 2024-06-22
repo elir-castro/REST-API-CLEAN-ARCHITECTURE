5bcf26a2ba1c1c50f7c4abb941de18629e25f807
whocastr <castro.luik@hotmail.com>
2024-06-22 19:46:50 -0300

    feat: adding jwt to authentication

--- BuberDinner/BuberDinner.Application/Authentication/AuthenticationService.cs
+++ BuberDinner/BuberDinner.Application/Authentication/AuthenticationService.cs
@@ -1,10 +1,28 @@
-﻿namespace BuberDinner.Application.Authentication
+﻿using BuberDinner.Application.Common.Interfaces.Authentication;
+
+namespace BuberDinner.Application.Authentication
 {
     public class AuthenticationService : IAuthenticationService
     {
+        private readonly IJwtTokenGenerator _jwtTokenGenerator;
+
+        public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator)
+        {
+            _jwtTokenGenerator = jwtTokenGenerator;
+        }
+
         public AuthenticationResult Register(string firstName, string lastName, string email, string password)
         {
-            return new AuthenticationResult(Guid.NewGuid(), firstName, lastName, email, "token");
+            //Check if user already exists
+
+            //Create user (generate unique ID)
+
+            //Create JWT token
+            var userId = Guid.NewGuid();
+
+            var token = _jwtTokenGenerator.GenerateToken(userId, firstName, lastName);
+
+            return new AuthenticationResult(Guid.NewGuid(), firstName, lastName, email, token);
         }
 
         public AuthenticationResult Login(string email, string password)
---
+++ BuberDinner/BuberDinner.Infraestructure/Services/DateTimeProvider.cs
@@ -0,0 +1,9 @@
+﻿using BuberDinner.Application.Common.Interfaces.Services;
+
+namespace BuberDinner.Infraestructure.Services
+{
+    public class DateTimeProvider : IDateTimeProvider
+    {
+        public DateTime UtcNow => DateTime.UtcNow;
+    }
+}
