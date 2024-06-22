cbd8dd23b7cd79e9fc2728fbbc68f18772bd825c
whocastr <castro.luik@hotmail.com>
2024-06-22 12:55:37 -0300

    feat: implementing AuthenticatorController

---
+++ BuberDinner/BuberDinner.Api/Controllers/AuthenticatorController.cs
@@ -0,0 +1,24 @@
+﻿using BuberDinner.Contracts.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuberDinner.Api.Controllers
+{
+    [Route("auth")]
+    [ApiController]
+    public class AuthenticatorController : ControllerBase
+    {
+        [HttpPost("register")]
+
+        public IActionResult Register(RegisterRequest request)
+        {
+            return Ok(request);
+        }
+
+        [HttpPost("login")]
+
+        public IActionResult Login(LoginRequest request)
+        {
+            return Ok(request);
+        }
+    }
+}
