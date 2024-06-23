7d029c50aafc4b6c00c2e53bde1317b7f7d64fdc
whocastr <castro.luik@hotmail.com>
2024-06-23 09:04:21 -0300

    feat: implementing a custom problem details factory

--- BuberDinner/BuberDinner.Api/Program.cs
+++ BuberDinner/BuberDinner.Api/Program.cs
@@ -1,5 +1,6 @@
 using BuberDinner.Application;
 using BuberDinner.Infraestructure;
+using Microsoft.AspNetCore.Diagnostics;
 
 var builder = WebApplication.CreateBuilder(args);
 {
@@ -7,11 +8,25 @@ var builder = WebApplication.CreateBuilder(args);
         .AddApplication()
         .AddInfraestructure(builder.Configuration);
 
+    //builder.Services.AddControllers(options => options.Filters.Add<ErrorHandlingFilterAttribute>());
     builder.Services.AddControllers();
+
+    //builder.Services.AddSingleton<ProblemDetailsFactory, BuberDinnerProblemDetailsFactory>();
+
 }
 
 var app = builder.Build();
 {
+    //app.UseMiddleware<ErrorHandleMiddleware>();
+    app.UseExceptionHandler("/error");
+
+    app.Map("/error", (HttpContext httpContext) =>
+    {
+        Exception? exception = httpContext.Features.Get<IExceptionHandlerFeature>()?.Error;
+
+        return Results.Problem();
+    });
+
     app.UseHttpsRedirection();
     app.MapControllers();
     app.Run();
